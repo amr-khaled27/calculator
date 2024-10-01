9622b9b5b388392893a79818e5e1d2e60a4ef27a
Amr Khaled <3moorkh2005@gmail.com>
2024-10-01 17:12:30 +0300

    added rotation

---
+++ src/rotate.js
@@ -0,0 +1,19 @@
+const frame = document.querySelector(".calc");
+
+document.addEventListener("mousemove", (e) => {
+  rotateElement(e, frame);
+});
+
+function rotateElement(event, element) {
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const middleX = window.innerWidth / 2;
+  const middleY = window.innerHeight / 2;
+
+  const offsetX = ((x - middleX) / middleX) * 7.5;
+  const offsetY = ((y - middleY) / middleY) * 7.5;
+
+  element.style.setProperty("--rotateX", offsetX + "deg");
+  element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+}
